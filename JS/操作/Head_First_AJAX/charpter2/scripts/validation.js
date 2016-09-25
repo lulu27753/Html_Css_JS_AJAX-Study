d8f96a5b47ae619aa7287d6eadeb15c43bbbf06c
lulu <lulu27753@163.com>
2016-09-25 11:38:21 +0800

    charpter2 and charpter5

--- JS/操作/Head_First_AJAX/charpter2/scripts/validation.js
+++ JS/操作/Head_First_AJAX/charpter2/scripts/validation.js
@@ -1,34 +1,1 @@
-window.onload = initPage;
-function initPage () {
-	document.getElementById("username").onblur = checkUsername;
-
-}
-function checkUsername () {
-	usernameRequest = createRequest();
-	if (!usernameRequest) {
-		alert("Unable to create request");
-	} else{
-		alert(0);
-		var username = document.getElementById("username").value;
-		alert(1);
-		url = "checkName.php?username=" + escape(username);
-		alert(2);
-		usernameRequest.onreadystatechange = showUsernameStatus;
-		alert(4);
-		usernameRequest.open("GET", url, true);
-		alert(3);
-		
-		usernameRequest.send(null);
-		alert(5);
-	};
-} 
-function showUsernameStatus () {
-	if (usernameRequest.readyState == 4 && usernameRequest.status == 200) {
-		alert(usernameRequest.responseText);
-		if (usernameRequest.responseText == "okay") {
-			alert(6);
-		} else{
-			alert("Sorry, that username is taken.");
-		};
-	};
-}
+window.onload = initPage;function initPage() {  document.getElementById("username").onblur = checkUsername;  document.getElementById("register").disabled = true;}function checkUsername() {  document.getElementById("username").className = "thinking";  request = createRequest();  if (request == null)    alert("Unable to create request");  else {    var theName = document.getElementById("username").value;    var username = escape(theName);    var url= "checkName.php?username=" + username;    request.onreadystatechange = showUsernameStatus;    request.open("GET", url, true);    request.send(null);  }}function showUsernameStatus() {  if (request.readyState == 4) {    if (request.status == 200) {      if (request.responseText == "okay") {        document.getElementById("username").className = "approved";        document.getElementById("register").disabled = false;      } else {        document.getElementById("username").className = "denied";        document.getElementById("username").focus();        document.getElementById("username").select();        document.getElementById("register").disabled = true;      }    }  }}
